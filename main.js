d4ed5d355cf01f149d51269fa1ec2ba969bf02d2
Habib Müküs <habibmukus@gmail.com>
2022-03-31 18:57:49 +0300

    server config

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ app.use('/person/referee',refereeRoute);
 app.use('/person/player',playerRoute);
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/localhost?connectTimeoutMS=1000", { useNewUrlParser: true }, (err) => {
-    if (err) return console.log(err.message);
-
-    console.log('connected succesfully');
-});
-
-// mongoose.connect("mongodb+srv://doadmin:394r78I65fe1FdkG@db-mongodb-nyc3-60882-2a167b96.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-nyc3-60882&tls=true&tlsCAFile=ca-certificate.crt", { useNewUrlParser: true }, (err) => {
+// mongoose.connect("mongodb://127.0.0.1:27017/localhost?connectTimeoutMS=1000", { useNewUrlParser: true }, (err) => {
 //     if (err) return console.log(err.message);
 
 //     console.log('connected succesfully');
 // });
+
+mongoose.connect("mongodb+srv://doadmin:394r78I65fe1FdkG@db-mongodb-nyc3-60882-2a167b96.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-nyc3-60882&tls=true&tlsCAFile=ca-certificate.crt", { useNewUrlParser: true }, (err) => {
+    if (err) return console.log(err.message);
+
+    console.log('connected succesfully');
+});
 
 app.listen(3000, () => console.log('listening on port 3000'));
